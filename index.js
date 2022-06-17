88e5a388fb4e7c9b84d5a1ac65f8db82f379ca72
kevin breid <kevinbreid@yahoo.com>
2022-06-16 17:33:19 -0700

    commit tree 3a3af773e310ea4e6d1e39bbdf915c2d7a13a4ba

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your solution in this file!
+const employee = {
+
+    name: 'Thom',
+    streetAddress: '2222 London' 
+}
+
+function updateEmployeeWithKeyandValue(obj, key, value){
+
+    return {...obj,[key]: value}
+
+
+
+}
+
+const newEmployee = updateEmployeeWithKeyandValue(employee, 'Sam', '11 Broadway')
+
+
